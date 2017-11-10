98c8f97feffd08e5c07d498dfa2976b62486ff05
missinglink <insomnia@rcpt.at>
2017-11-10 18:59:08 +0100

    formatting

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ OUTDIR=${OUTDIR:-'/data/wof-spatialite'}
 mkdir -p "${OUTDIR}"
 rm -rf "${OUTDIR}/wof.sqlite"
 
+# tessellation options
+LEVEL=50
+COMPLEXITY=100
+
 # default coords
 LON=${LON:-'50'}
 LAT=${LAT:-'1'}
@@ -27,9 +31,9 @@ time docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spa
 
 echo '-- create tiles --'
 time docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' tile_init
-time docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' tile_all "50" "100"
+time docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' tile_all "${LEVEL}" "${COMPLEXITY}"
 
 echo '-- run point-in-polygon tests --'
-docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' pip "$LON" "$LAT"
-docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' pipfast "$LON" "$LAT"
-docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' piptile "$LON" "$LAT"
+docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' pip "${LON}" "${LAT}"
+docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' pipfast "${LON}" "${LAT}"
+docker run -e 'DB=/out/wof.sqlite' -v "${OUTDIR}:/out" 'missinglink/wof-spatialite' piptile "${LON}" "${LAT}"
